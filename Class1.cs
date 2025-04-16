18214a0f4489df6c0c09c2fc7e28749f270821f5
Al2yk <sharonovaas@mail.ru>
2025-04-16 11:06:42 +0300

    division

--- Class1.cs
+++ Class1.cs
@@ -14,5 +14,12 @@ namespace mathFunc
 
         public static double Multiply(double a, double b) => a * b;
 
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Попытка деления на ноль!");
+            return a / b;
+        }
+
     }
 }
